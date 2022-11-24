fbda0c0e18748104d238058c292f9cebfbe2dcff
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-24 03:33:03 +0200

    squash!

--- tests/nikolaxhristov/pipeline/index.spec.ts
+++ tests/nikolaxhristov/pipeline/index.spec.ts
@@ -8,33 +8,44 @@ const execP = promisify(exec);
 
 jest.setTimeout(30000000);
 
-describe("compression", () => {
-	test("smaller directory", async () => {
-		await new pipeline({
-			path: new Map([
-				[
-					"./samples/nikolaxhristov/pipeline/input",
-					"./samples/nikolaxhristov/pipeline/output",
-				],
-			]),
-		}).compress();
+const compress = async () => {
+	await new pipeline({
+		path: new Map([
+			[
+				"./samples/nikolaxhristov/pipeline/input",
+				"./samples/nikolaxhristov/pipeline/output",
+			],
+		]),
+	}).compress();
+};
+
+const getSize = async (path: string) =>
+	parseInt((await execP(`du -s ${path} | cut -f1`)).stdout);
+
+describe("pipeline", () => {
+	test("compress", async () => {
+		try {
+			await fs.promises.rm("./samples/nikolaxhristov/pipeline/output/", {
+				recursive: true,
+			});
+		} catch (_error) {}
+
+		await compress();
 
 		expect(
-			parseInt(
-				(
-					await execP(
-						"du -s ./samples/nikolaxhristov/pipeline/output/ | cut -f1"
-					)
-				).stdout
-			)
+			await getSize("./samples/nikolaxhristov/pipeline/output")
 		).toBeLessThan(
-			parseInt(
-				(
-					await execP(
-						"du -s ./samples/nikolaxhristov/pipeline/input/ | cut -f1"
-					)
-				).stdout
-			)
+			await getSize("./samples/nikolaxhristov/pipeline/input")
+		);
+	});
+
+	test("compress but with cache", async () => {
+		await compress();
+
+		expect(
+			await getSize("./samples/nikolaxhristov/pipeline/output")
+		).toBeLessThan(
+			await getSize("./samples/nikolaxhristov/pipeline/input")
 		);
 	});
 });
