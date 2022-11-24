60912319066926d10b4c60eea1443d26063b2b22
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-24 21:29:23 +0200

    squash!

--- dist/bench/nikolaxhristov/pipeline/compression,no-cache.js
+++
@@ -1,1 +0,0 @@
-import*as r from"fs";import o from"./lib/compression.js";var i=async()=>{try{await r.promises.rm("./samples/nikolaxhristov/pipeline/output/",{recursive:!0})}catch{}await o()};export{i as default};
---
+++ dist/bench/nikolaxhristov/pipeline/compression.no-cache.js
@@ -0,0 +1,1 @@
+import*as r from"fs";import i from"./lib/compression.js";try{await r.promises.rm("./samples/nikolaxhristov/pipeline/output/",{recursive:!0})}catch{}await i();
