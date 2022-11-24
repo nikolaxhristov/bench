72b60e8456234f6835ff0c538f793b267d359e4e
Nikola Hristov <nikola@nikolahristov.tech>
2022-11-24 04:47:39 +0200

    squash!

--- bench.sh
+++ bench.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-mkdir -p ./bench/nikolarhristov/pipeline/
-touch ./bench/nikolarhristov/pipeline/cache.md
+mkdir -p ./bench/nikolaxhristov/pipeline/
+touch ./bench/nikolaxhristov/pipeline/cache.md
 
-mkdir -p ./bench/nikolarhristov/pipeline/
-touch ./bench/nikolarhristov/pipeline/no-cache.md
+mkdir -p ./bench/nikolaxhristov/pipeline/
+touch ./bench/nikolaxhristov/pipeline/no-cache.md
 
 hyperfine \
 	--runs 1 \
-	--export-markdown ./bench/nikolarhristov/pipeline/no-cache.md \
+	--export-markdown ./bench/nikolaxhristov/pipeline/no-cache.md \
 	'pnpm run test:nikolaxhristov:pipeline:compression:no-cache'
 
 hyperfine \
 	--runs 1 \
-	--export-markdown ./bench/nikolarhristov/pipeline/cache.md \
+	--export-markdown ./bench/nikolaxhristov/pipeline/cache.md \
 	'pnpm run test:nikolaxhristov:pipeline:compression:cache'
 
 {
 	printf '# benchmarks\n'
-	printf '\n## @nikolarhristov/pipeline no-cache\n'
-	cat ./bench/nikolarhristov/pipeline/no-cache.md
-	printf '\n## @nikolarhristov/pipeline cache\n'
-	cat ./bench/nikolarhristov/pipeline/cache.md
+	printf '\n## @nikolaxhristov/pipeline no-cache\n'
+	cat ./bench/nikolaxhristov/pipeline/no-cache.md
+	printf '\n## @nikolaxhristov/pipeline cache\n'
+	cat ./bench/nikolaxhristov/pipeline/cache.md
 } >README.md
